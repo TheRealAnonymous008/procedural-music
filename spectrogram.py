ea31e33160707c7218f77f6106dcfe343836d554
JoaquinDelaRosa <62501229+TheRealAnonymous008@users.noreply.github.com>
2022-12-19 13:01:26 +0800

    chore: refactor

--- spectrogram.py
+++ spectrogram.py
@@ -42,5 +42,14 @@ def get_frequencies(duration_start_seconds, duration_end_seconds, audio_data, sa
     return N, transform, fft_samples
 
 
+def plot_frequencies(duration_start_seconds, duration_send_seconds, audio_data, samples_per_second):
+    N, transform, fft_samples = get_frequencies(0, 1, data, sampling_rate)
+
+    plt.xlim(0, 8000)
+    plt.plot(fft_samples, 2.0 / N * np.abs(transform[:N // 2]), alpha=0.5)
+    plt.show()
+
+
 if __name__ == '__main__':
-    print("Hello")
+    sampling_rate, data = read_file('data/piano.mp3')
+    plot_frequencies(1, 1.5, data, sampling_rate)
