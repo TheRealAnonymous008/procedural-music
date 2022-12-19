2c99a0a4d2cd7a192ed1df47523bddd992ca408c
JoaquinDelaRosa <62501229+TheRealAnonymous008@users.noreply.github.com>
2022-12-19 20:12:01 +0800

    add: pyin using librosa

--- pitch_analysis.py
+++ pitch_analysis.py
@@ -1,19 +1,22 @@
-# TODO: Implement the YIN Pitch prediction algorithm
-from matplotlib import pyplot as plt
-from statsmodels.tsa.stattools import acf
+# Using the pYIN algorithm implemented by the librosa package.
 
-from frequency_analysis import get_samples
-from midiio import read_file
-
-
-def plot_acf(samples, window):
-    autocorr = acf(samples, adjusted=True, nlags=window)
-    plt.plot(autocorr)
-    plt.show()
-
+import librosa
+from librosa import pyin, amplitude_to_db, stft, display, times_like
+import matplotlib.pyplot as plt
+import numpy as np
 
 if __name__ == '__main__':
-    sampling_rate, data = read_file('data/piano.mp3')
+    data, sample_rate = librosa.load('data/piano.mp3')
 
-    samples = get_samples(42, 42.1, data, sampling_rate)[:, 1]
-    plot_acf(samples, 1000)
+    print("This ran")
+    f0, voiced_flags, voiced_probs = pyin(data, fmin=20, fmax=10000)
+    times = times_like(f0)
+
+    D = amplitude_to_db(np.abs(stft(data)), ref=np.max)
+    fig, ax = plt.subplots()
+    img = display.specshow(D, x_axis='time', y_axis='log', ax=ax)
+    ax.set(title='pYIN fundamental frequency estimation')
+    fig.colorbar(img, ax=ax, format="%+2.f dB")
+    ax.plot(times, f0, label='f0', color='cyan', linewidth=3)
+    ax.legend(loc='upper right')
+
