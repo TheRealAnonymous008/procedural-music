c5afe20864bbba06fb33facecb4d1a3b889b40b4
JoaquinDelaRosa <80933658+JoaquinDelaRosa@users.noreply.github.com>
2022-12-20 09:49:18 +0800

    add: pitch embedding

--- frequency_analysis.py
+++ frequency_analysis.py
@@ -1,4 +1,5 @@
 from scipy import fft
+from scipy.signal import welch
 
 
 def get_samples(duration_start_seconds, duration_end_seconds, audio_data, samples_per_second):
@@ -7,10 +8,11 @@ def get_samples(duration_start_seconds, duration_end_seconds, audio_data, sample
     return audio_data[int(sample_start): int(sample_end)]
 
 
-def get_frequencies(duration_start_seconds, duration_end_seconds, audio_data, samples_per_second):
-    samples = get_samples(duration_start_seconds, duration_end_seconds, audio_data, samples_per_second)
-    transform = fft.fft(samples)
-    N = len(samples)
-    T = 1.0 / samples_per_second
-    fft_samples = fft.fftfreq(N, T)[:N//2]
-    return N, transform, fft_samples
+def get_frequencies(audio_data, samples_per_second):
+    f, s  = welch(audio_data, 
+        fs=samples_per_second, 
+        detrend=False,
+        nfft=512, 
+    )
+
+    return f, s
