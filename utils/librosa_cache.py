4011a09deffc1cb85ecc7e79369311f223ebc985
JoaquinDelaRosa <80933658+JoaquinDelaRosa@users.noreply.github.com>
2022-12-22 12:50:32 +0800

    clean!!

--- utils/librosa_cache.py
+++
@@ -1,9 +0,0 @@
-import os
-
-## This must be called before anything else.
-def enable_cache():
-    os.environ['LIBROSA_CACHE_DIR'] = '/tmp/librosa_cache'
-
-import librosa 
-def purge_cache():
-    librosa.cache.clear()
--- utils/read_audio.py
+++
@@ -1,5 +0,0 @@
-from utils.constants import *
-import librosa
-
-def read_file(path : str): 
-    return librosa. load(path, sr= 44100, mono=DEFAULT_SAMPLING_RATE)
